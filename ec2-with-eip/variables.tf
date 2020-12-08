e7f0744434b2726af6890d8e56731d6172bdcddc
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2020-12-08 21:46:53 +0530

    Create variables.tf

---
+++ ec2-with-eip/variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  description = "Region in which EC2 Instance to be launched"
+  type = string
+  default = "us-east-2"
+}
+variable "availability_zone" {
+  type = list
+  default = ["us-east-2a", "us-east-2b", "us-east-2c"]
+}
+variable "ami" {
+  type = map
+  default = {
+    us-east-1 = "ami-04d29b6f966df1537"
+    us-east-2 = "ami-09558250a3419e7d0"
+    us-west-1 = "ami-08d9a394ac1c2994c"
+    us-west-2 = "ami-0e472933a1395e172"
+  }
+}
