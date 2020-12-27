244f5735b2dec3cf8e61beaddde00fab9f294b29
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2020-12-27 21:41:50 +0530

    Delete variables.tf

--- create-ebs-attached-to-running-ec2/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-description = "Region into which to create the AWS Resource"
-type = string
-default = "us-east-2"
-}
-variable "volume_size" {
-description = "Provide the EBS Volume Size to be created"
-type = number
-}
