ef6f54e66e9f5dfd7ca77ded41dfe1081d3a92e4
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2021-05-19 21:06:08 +0530

    Update sqs.tf

--- sqs/sqs.tf
+++ sqs/sqs.tf
@@ -32,8 +32,3 @@ resource "aws_sqs_queue" "terraform_queue" {
     Environment = "production"
   }
 }
-
-resource "aws_lambda_event_source_mapping" "example" {
-  event_source_arn = aws_sqs_queue.terraform_queue.arn
-  function_name    = aws_lambda_function.test_lambda.arn 
-}
