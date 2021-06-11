cfaeb9aa70f7b50f69bcfdce5fc776eec27c23ff
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2021-06-11 10:59:45 +0530

    Add files via upload

---
+++ ec2_with_provisioner/ec2_provisioner.tf
@@ -0,0 +1,67 @@
+resource "aws_instance" "demo_instance" {
+  ami           = var.provide_ami["us-east-2"]
+  instance_type = var.instance_type[0]
+  key_name = "key"
+#  iam_instance_profile = aws_iam_instance_profile.ec2_iam_profile.name  
+  monitoring = true
+  security_groups = var.security_group
+  subnet_id = var.subnet_id
+  associate_public_ip_address = true
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 20
+    delete_on_termination = true
+    encrypted = true
+    kms_key_id = "d4faa181-32bd-48f4-987f-3fb9160caec7"
+  }
+  user_data =   <<-EOF
+                #!/bin/bash
+                /usr/sbin/useradd -s /bin/bash -m ritesh;
+                mkdir /home/ritesh/.ssh;
+                chmod -R 700 /home/ritesh;
+                echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJlKY/Wv7MMhv0sMnkQ863LyOUn9sptj/H3lU2w3Dos/KkoLUVqvGLIT6wF0muuZkJp4F0/kkvgwS6upfghdIUmqsNhlIGvczLF9+Ht4tR5YD12pw9zN4NcikT667VqKJeVM83NLeOX8imgca2rbUjSUOO2sjgLJa60/PQkcKiMFAfbMYkE2tYTQCW5s3AfoJB/7wPg/X+bcLwGJjTwwGtXF5NcgJ0um8qCdoXzD6dMeApJeUq73qez9oqz9c+p9VoPkWgxV+A5OyNup1ydCxqte7eJGt6ay0/fTtm4nT9j0GAnnaH31kt9V6W4xJxKRjW2syJ6KJpBXmt4ZLnDbGq8nTDN/7z6dMWR7Ai2pAGgYcMZcizVK8wx5GG9C4+kQR1nRVVqhB1bdWJjXVIUGnry826qnrtImJyFvtrAuFQXsnC2noBp+0aCLeFNT9TtxDwZLGJ33xgrPcVvCyxL8rlUyxrIMH8CGXRXAIgDRv3Nu0CTp6q8IkCNt+ZzGC6//U= ritesh@DESKTOP-2UQTKO7" >> /home/ritesh/.ssh/authorized_keys;
+                chmod 600 /home/ritesh/.ssh/authorized_keys;
+                chown ritesh:ritesh /home/ritesh/.ssh -R;
+                echo "ritesh  ALL=(ALL)  NOPASSWD: ALL" > /etc/sudoers.d/ritesh;
+                chmod 440 /etc/sudoers.d/ritesh;
+#                sudo yum update -y
+#                sudo yum install -y httpd
+#                sudo systemctl start httpd
+#                sudo systemctl enable httpd
+#                echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+                EOF
+  lifecycle {
+    prevent_destroy = false
+#    ignore_changes = [ ami ]
+}
+  tags = {
+    Name = var.instance_name
+    Environment = var.environment
+  }
+  
+  provisioner "file" {
+    source    = "index.html"
+    destination = "/tmp/index.html"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+                "sudo yum install httpd -y",
+                "sudo service httpd start",
+                "sudo chkconfig httpd on",
+                "sudo cp /tmp/index.html /var/www/html/",
+                "sudo echo 'HallaBol!!!' > /tmp/test.txt"
+   ]
+  }
+  connection {
+    user = "ec2-user"
+    private_key = file("/home/ec2-user/key.pem")
+    host = aws_instance.demo_instance.public_ip
+  }
+
+}
+
+  resource "aws_eip" "eip_associate" {
+  instance = aws_instance.demo_instance.id
+  vpc      = true
+}
---
+++ ec2_with_provisioner/variables.tf
@@ -0,0 +1,38 @@
+variable "region" {
+default = "us-east-2"
+}
+variable "provide_ami" {
+description = "Provide the AMI ID for the EC2 Instance"
+type = map
+default = {
+    us-east-1 = "ami-04d29b6f966df1537"
+    us-east-2 = "ami-09558250a3419e7d0"
+    us-west-1 = "ami-08d9a394ac1c2994c"
+    us-west-2 = "ami-0e472933a1395e172"
+}
+}
+variable "instance_type" {
+description = "Provide the Instance type of the EC2 Instance"
+type = list
+default = ["t3.micro", "t3.small", "t3.medium", "t3.large", "t3.xlarge", "t3.2xlarge"]
+}
+variable "security_group" {
+description = "Provide the Security Group ID of the security Groups"
+type = list
+default = [ "sg-06af727dd120cfe65" ]
+}
+variable "subnet_id" {
+description = "Provide the subnet Id of the subnet"
+type = string
+default = "subnet-070ec2c4ff53f26c8"
+}
+variable "instance_name" {
+description = "Provide the Instance Name"
+type = string
+default = "MyInstance-1"
+}
+variable "environment" {
+description = "Provide the Environment Name"
+type = string
+default = "Dev"
+}
