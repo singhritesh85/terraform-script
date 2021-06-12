558232705ff7384c0230abbc0dcff366db7a2f5c
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2021-06-12 14:47:56 +0530

    Add files via upload

---
+++ automate_alb_asg_using_packer/newvariables.tf
@@ -0,0 +1,1 @@
+variable "AMI_ID" { default = "ami-0823e8e5d5171a8dd" }
---
+++ automate_alb_asg_using_packer/test.sh
@@ -0,0 +1,6 @@
+NEW=`/opt/packer build template.json | tail -2`
+AMI_ID=`echo $NEW|cut -d ':' -f2`
+echo $AMI_ID
+echo 'variable "AMI_ID" { default = "'${AMI_ID//[[:blank:]]/}'" }' > newvariables.tf
+terraform init
+terraform apply -auto-approve
