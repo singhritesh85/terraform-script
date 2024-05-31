0d0d31d37e0ecbe1c986a2861ee2385a67a86265
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2024-05-31 12:55:05 +0530

    Update test.sh

--- automate-alb-autoscalinggroup-using-packer/module/test.sh
+++ automate-alb-autoscalinggroup-using-packer/module/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+/opt/packer plugins install github.com/hashicorp/amazon
+/opt/packer plugins install github.com/hashicorp/ansible
 TEST=`/opt/packer build template.json | tail -2`
 AMI_ID=`echo $TEST|cut -d ":" -f2`
 echo $AMI_ID
